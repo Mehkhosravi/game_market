383981e965fccd879b6637215749f7600a0af660
Mehrnaz <your.email@example.com>
2025-09-17 14:57:03 +0200

    install ChakraUI with button

---
+++ game-sell-website/src/App.tsx
@@ -0,0 +1,26 @@
+
+
+import { Button, HStack } from "@chakra-ui/react"
+import { RiArrowRightLine, RiMailLine } from "react-icons/ri"
+
+const Demo = () => {
+  return (
+    <HStack>
+      <Button colorPalette="teal" variant="solid">
+        <RiMailLine /> Email
+      </Button>
+      <Button colorPalette="teal" variant="outline">
+        Call us <RiArrowRightLine />
+      </Button>
+    </HStack>
+  )
+}
+
+
+
+function App() {
+ return <Demo />
+  
+}
+
+export default App;
