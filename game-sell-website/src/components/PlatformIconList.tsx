21166dc30dd9e227d2d100b4abeb835a239395c2
Mehrnaz <your.email@example.com>
2025-09-24 13:58:19 +0200

    set platform Icon

---
+++ game-sell-website/src/components/PlatformIconList.tsx
@@ -0,0 +1,41 @@
+import {FaAndroid,FaLinux,FaApple, FaWindows, FaXbox,FaPlaystation} from "react-icons/fa";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { MdPhoneIphone } from "react-icons/md";
+import { HStack, Icon } from "@chakra-ui/react";
+import type { Platform } from '../hooks/useGame';
+import type { IconType } from "react-icons";
+
+interface Props{
+    platform:Platform[];
+}
+export const PlatformIconList = ({platform}:Props) => {
+    const iconMap:{[key:string]:IconType}={
+        playstation: FaPlaystation,
+        xbox: FaXbox,
+        nintendo: SiNintendo,
+        mac: FaApple,
+        linux: FaLinux,
+        web: BsGlobe,
+        pc:FaWindows,
+        android: FaAndroid,
+        ios: MdPhoneIphone
+    }
+
+    function normalizeSlug(slug: string): string {
+    if (slug.startsWith("playstation")) return "playstation";
+    if (slug.startsWith("xbox")) return "xbox";
+    if (slug === "macos") return "mac"; // API uses "macos", you mapped "mac"
+    if (slug.startsWith("nintendo")) return "nintendo";
+    return slug; // keep as is if already matches
+    }
+  return (
+    <HStack>
+        {platform.map((p) => {
+        const normalized = normalizeSlug(p.slug);
+        const IconComponent = iconMap[normalized];
+        return IconComponent ? <Icon as={IconComponent} key={p.id} /> : null;
+      })}
+    </HStack>
+  )
+};
