5d7c4cf4b88079205a6f7212814d92081ca407fa
Mehrnaz <mehrnazz.khosravi@gmail.com>
2026-07-15 23:57:42 +0200

    Add Env API Client

--- game-sell-website/src/services/api-client.ts
+++ game-sell-website/src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params:{
-        key:"36ef63dcdbb5487992b3d32850b68970"
+        key: import.meta.env.VITE_RAWG_KEY
     }    
 });
